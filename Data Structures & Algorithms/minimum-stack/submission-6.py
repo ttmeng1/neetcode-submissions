59aba72ad38f16679fd3af1671f0b22508f97027
Tiffany Meng <ttmeng@ad.unc.edu>
2026-07-29 17:51:39 -0400

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.min_stack == []:
+            self.min_stack.append(val)
+        else:
+            self.min_stack.append(min(self.min_stack[-1], val))
+
+    def pop(self) -> None:
+        self.stack.pop(-1)
+        self.min_stack.pop(-1)
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
